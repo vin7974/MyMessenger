5d501bb5b2a8a973a2b0ae5eb84cea203346d416
Vinod <vinod7974@gmail.com>
2018-04-13 19:29:46 +0530

    Intent(navigating to next acitivity)

--- app/src/main/java/com/example/vinod/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/vinod/mymessenger/CreateMessageActivity.java
@@ -2,6 +2,8 @@ package com.example.vinod.mymessenger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class CreateMessageActivity extends Activity {
 
@@ -10,4 +12,11 @@ public class CreateMessageActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
     }
+    public void onSendMessage(View view)
+    {
+        Intent intent = new Intent(this, ReceiveMessageAcitivity.class);
+        startActivity(intent);
+
+    }
+
 }
---
+++ app/src/main/java/com/example/vinod/mymessenger/ReceiveMessageAcitivity.java
@@ -0,0 +1,13 @@
+package com.example.vinod.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReceiveMessageAcitivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message_acitivity);
+    }
+}
