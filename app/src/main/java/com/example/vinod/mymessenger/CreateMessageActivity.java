f51b4922b729fa617dab1e13a9ad0e1e908dbe65
Vinod <vinod7974@gmail.com>
2018-04-16 17:30:15 +0530

    message passing

--- app/src/main/java/com/example/vinod/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/vinod/mymessenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -14,7 +15,12 @@ public class CreateMessageActivity extends Activity {
     }
     public void onSendMessage(View view)
     {
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageAcitivity.class);
+        intent.putExtra(ReceiveMessageAcitivity.EXTRA_MESSAGE, messageText);
+        // another messsage with different key word
+        intent.putExtra("SampleKey", messageText);
         startActivity(intent);
 
     }
--- app/src/main/java/com/example/vinod/mymessenger/ReceiveMessageAcitivity.java
+++ app/src/main/java/com/example/vinod/mymessenger/ReceiveMessageAcitivity.java
@@ -2,12 +2,20 @@ package com.example.vinod.mymessenger;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class ReceiveMessageAcitivity extends Activity {
 
+    public  static  final  String EXTRA_MESSAGE = "Static Message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message_acitivity);
+
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
     }
 }
